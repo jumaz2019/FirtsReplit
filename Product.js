313791793344cb35b53666955b26fb56e8a6a9c6
Juan Manuel Manuel <48811352+jumaz2019@users.noreply.github.com>
2022-07-03 22:37:54 +0000

    Nueva clase Product

---
+++ Product.js
@@ -0,0 +1,6 @@
+export class Product {
+  constructor(name, valor){
+     this.name = name
+     this.valor = valor
+  }
+}
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import {} from './User.js'
+import {User} from './User.js'
+import {Product} from './Product.js'
+
 const juan = new User('Juan', 'Zapata')
 console.log(juan)
+console.log(new Product('Laptop', 1500))
